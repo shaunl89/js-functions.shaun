3ef3f06710f9b3e43ca62761e7a07283e88bd65f
Shaun Loh <loh.shaun89@gmail.com>
2017-06-22 11:26:38 +0800

    made some random changes

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
 console.log('linked to my html')
 
-console.log('linked to my html')
+console.log('trying some changesgi')
 
-function pausecomp (millis) {
-  var date = new Date()
-  var curDate = null
-
-  while (curDate - date < millis) {
-    curDate = new Date()
-  }
-  //alert('done pausing')
-}
-
-pausecomp(3000)
+// function pausecomp (millis) {
+//   var date = new Date()
+//   var curDate = null
+//
+//   while (curDate - date < millis) {
+//     curDate = new Date()
+//   }
+//   //alert('done pausing')
+// }
+//
+// pausecomp(3000)
